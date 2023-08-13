962917dee36e91c77579c67f8a5d4148b6bf47c6
Bibek <bibek.roman1@gmail.com>
2023-08-13 16:44:58 +0530

    JWTAuthenticationFilter and Security Configuration added

---
+++ src/main/java/com/example/jwt/security/SecurityConfig.java
@@ -0,0 +1,36 @@
+package com.example.jwt.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.stereotype.Component;
+
+@Component
+public class SecurityConfig {
+
+    @Autowired
+    private JwtHelper jwtHelper;
+    @Autowired
+    private JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint;
+    @Autowired
+    private JWTAuthenticationFilter jwtAuthenticationFilter;
+
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.csrf(csrf->csrf.disable())
+                .cors(cors->cors.disable())
+                .authorizeHttpRequests(auth->auth
+                        .requestMatchers("/home/**")
+                        .authenticated()
+                        .requestMatchers("/auth/login/**")
+                        .permitAll()
+                        .anyRequest()
+                        .authenticated())
+                .exceptionHandling(ex->ex.authenticationEntryPoint(jwtAuthenticationEntryPoint))
+                .sessionManagement(session->session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+
+        http.addFilterBefore(jwtAuthenticationFilter,UsernamePasswordAuthenticationFilter.class);
+        return http.build();
+    }
+}
